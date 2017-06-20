da4965e61011e941b729fdba644b97e1ead920d9
Nickernator <nickwindt@hotmail.nl>
2017-06-20 16:40:04 +0200

    commit'

--- src/main/java/ipass/eventapp/persistence/BaseDAO.java
+++ src/main/java/ipass/eventapp/persistence/BaseDAO.java
@@ -4,12 +4,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class BaseDAO {
-	//private SessionFactory factory = new Configuration().setProperty("hibernate.connection.url", System.getenv("DATABASE_URL")).configure().buildSessionFactory();
+	private SessionFactory factory = new Configuration().setProperty("hibernate.connection.url", System.getenv("DATABASE_URL")).configure().buildSessionFactory();
 	
 	protected final SessionFactory getConnection(){
-		Configuration cfg = new Configuration();
-		cfg.setProperty("hibernate.connection.url", System.getenv("DATABASE_URL"));
-		return cfg.buildSessionFactory() ;
+		return factory ;
 	}
 	
 }
